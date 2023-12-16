dd10c1ee096021821762353dcf2a1739fbc4340b
Ayush Nair <64899298+couchundergrad@users.noreply.github.com>
2023-12-16 21:40:55 +0530

    Added a nav-bar to sign-up page

---
+++ signup.php
@@ -0,0 +1,43 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <title>Sign-up Page</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Bootstrap demo</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+<style>
+.nav{
+  display: flex;
+  justify-content: space-between;
+}
+
+</style>
+
+  </head>
+  <body>
+<div class=sign-nav-flex> 
+  <ul class="nav">
+  <li class="nav-item">
+    <a class="nav-link active" aria-current="page" href="http://127.0.0.1:5500/Main-Menu.html">Back To Main-Menu</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="http://localhost/loginsystem/login.php">Go To Login Page</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Link</a>
+  </li>
+  
+</ul>
+</div>
+ 
+<div class="container">
+
+<h1 class="text-center" >Sign Up To Our Website Now!</h1>
+
+</div>
+    
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+  </body>
+</html>
